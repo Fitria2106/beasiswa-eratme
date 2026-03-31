436e0861e80f2275b5c36c07393cd38930c8c451
Fitria Yosefina <fitria.yosefina@students.utdi.ac.id.com>
2026-03-31 20:06:51 +0700

    Add compiled assets

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
+
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-5">
