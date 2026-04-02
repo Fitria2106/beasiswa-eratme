d39103cfbe00f8d4fb0fc0993ad4fd0fffd52f2d
Fitria Yosefina <fitria.yosefina@students.utdi.ac.id.com>
2026-04-02 07:07:51 +0700

    Membuat ulang tabel reports

---
+++ database/migrations/2026_04_02_000615_create_reports_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('nama_buku');
+            $table->integer('harga');
+            $table->text('ringkasan_buku'); // Kolom yang tadi kita bahas ada di sini
+            $table->string('foto_struk')->nullable(); // Tambahan jika butuh upload foto
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reports');
+    }
+};
